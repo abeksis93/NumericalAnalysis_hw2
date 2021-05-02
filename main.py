9d94fc7a2385c5894eeea8ce65636be30e4caa69
harelm4 <harelmx@gmail.com>
2021-05-02 19:35:20 +0300

    commit one

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import math
+from random import random
 
 import gaussian as gaussian
-import matplotlib as plt
+import matplotlib.pl as plt
 import numpy as np
 
 
@@ -9,11 +10,33 @@ def gaussian(x, var, miu):
     return 1 / (math.sqrt(var * 2 * math.pi)) * np.exp(-0.5 * np.power((x - miu), 2)/var)
 
 
-def g(X, m, ul, r, w, d, miu):
-    index = 0
-    gResult = 1
+def g(X,m,ul,r,w,d,miu):
+    """
+    X-list of decision variables
+    m- Number of local optima
+    ul-Interval span of side constraints
+    r - Ratio of local optima to global optima(=1)
+    w- w*ul is the variance
+    d- all peaks must be in range [-d*ul/2,d*ul/2]
+
+    """
+    index=0
+    gResult=1
     for x in X:
-        gResult *= r*gaussian(x, w*ul, miu[index])
-        index += 1
+        gResult *= gaussian(x,w*ul,miu[index])
+        index+=1
+
+def G(X,m,ul,r,w,d,miu,Wi):
+    """
+    X-list of decision variables
+    m- Number of local optima
+    ul-Interval span of side constraints
+    r - Ratio of local optima to global optima(=1)
+    w- w*ul is the variance
+    d- all peaks must be in range [-d*ul/2,d*ul/2]
+    miu- list of  an average point of an n dimensional Gaussian distribution function
+    Wi - list of m+1 weights for each g_i
+    """
+
 
 
