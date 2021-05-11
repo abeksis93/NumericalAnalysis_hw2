3748e88675ef252d2bd7b10af352bdaa48448502
harelm4 <harelmx@gmail.com>
2021-05-11 18:40:03 +0300

    almost finnshed first part

--- example.py
+++ example.py
@@ -1,27 +1,34 @@
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+# import numpy as np
+# from scipy.stats import multivariate_normal
+# mu_x = 0
+# variance_x = 3
+# #
+# mu_y = 0
+# variance_y = 15
+#
+#
+# x = np.linspace(-10,10,500)
+# y = np.linspace(-10,10,500)
+# X,Y = np.meshgrid(x,y)
+# print(X.shape+(2,))
+# pos = np.empty(X.shape + (2,))
+# pos[:, :, 0] = X; pos[:, :, 1] = Y
+# rv = multivariate_normal([mu_x, mu_y], [[variance_x, 0], [0, variance_y]])
+#
+#
+# fig = plt.figure()
+# ax = fig.gca(projection='3d')
+# ax.plot_surface(X, Y, rv.pdf(pos),cmap='viridis',linewidth=0)
+# ax.set_xlabel('X axis')
+# ax.set_ylabel('Y axis')
+# ax.set_zlabel('Z axis')
+# plt.show()
+# #
 import numpy as np
-from scipy.stats import multivariate_normal
-mu_x = 0
-variance_x = 3
-#
-mu_y = 0
-variance_y = 15
 
-
-x = np.linspace(-10,10,500)
-y = np.linspace(-10,10,500)
-X,Y = np.meshgrid(x,y)
-print(X.shape+(2,))
-pos = np.empty(X.shape + (2,))
-pos[:, :, 0] = X; pos[:, :, 1] = Y
-rv = multivariate_normal([mu_x, mu_y], [[variance_x, 0], [0, variance_y]])
-
-
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-ax.plot_surface(X, Y, rv.pdf(pos),cmap='viridis',linewidth=0)
-ax.set_xlabel('X axis')
-ax.set_ylabel('Y axis')
-ax.set_zlabel('Z axis')
-plt.show()
-#
+nx, ny = (3, 2)
+x = np.linspace(0, 1, 3)
+y = np.linspace(0, 2, 3)
+xv, yv = np.meshgrid(x, y)
+print(yv)
--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import random
 
 # import gaussian as gaussian
 import matplotlib.pyplot as plt
+import numpy
 import numpy as np
+from mealpy.math_based.HC import BaseHC
+from mealpy.swarm_based.GWO import BaseGWO
 from scipy.stats import multivariate_normal
 
 
@@ -35,7 +38,7 @@ def multivar_gaussian(variables, variance_list, miu_list):
     return matrix
 
 
-def G(m, Wi, variables, variance_list, miu_list):  # todo: set peaks in d range
+def G(m, variance_list, miu_list):
     """
     X-list of decision variables
     m- Number of local optima
@@ -50,24 +53,10 @@ def G(m, Wi, variables, variance_list, miu_list):  # todo: set peaks in d range
     max_val = 0
     for i in range(m + 1):
         g_lst.append(multivariate_normal(miu_list[i], np.diag(variance_list[i])))
-        # for i in range(0, len(variables[0])):
-        #     for j in range(0, len(variables[0])):
-        # g_lst.append(multivar_gaussian(variables,variance_list,miu_list))
-        # g_lst[g][i][j] = g_lst[g][i][j]*Wi[g];
         if max_val < g_lst[i].pdf(miu_list[i]):
             max_val = g_lst[i].pdf(miu_list[i])
             f = g_lst[i]
 
-    # max_matrix = []
-    # col=[]
-    # valueList=[]
-    # for i in range(0, len(variables[0])):
-    #     for j in range(0, len(variables[0])):
-    #         for g in range(0,m+1):
-    #             valueList.append(g_lst[g][i][j])
-    #
-    #         col.append(max(valueList))
-    #     max_matrix.append(col)
 
     return lambda x: sum(
         r * (1 - 0.05 * i) * g_lst[i].pdf(x) if g_lst[i] != f else f.pdf(x) / max_val for i in range(m + 1))
@@ -95,29 +84,100 @@ for i in range(0, 6):
 
 variance_list = []
 for i in range(0, m + 1):
-    miu_list.append([random.randrange(-(ul/2)*d,(ul/2)*d), random.randrange(-(ul/2)*d,(ul/2)*d)])
-    variance_list.append([np.random.uniform(0, ulList[random.randint(0, 3)] * wList[random.randint(0, 3)]),
-                          np.random.uniform(0, ulList[random.randint(0, 3)] * wList[random.randint(0, 3)])])
+    miu_list.append([np.random.uniform(-d*ul/2,d*ul/2) for i in range(n)])
+    variance_list.append([np.random.uniform(w*ul/10,w*ul) for i in range(n)])
 
 variables = []
 for i in range(0, n):
     variables.append(np.linspace(-ul / 2, ul / 2, 1000))
 
 # function
-z = G(m, Wlist, variables, variance_list, miu_list)
+z = G(m, variance_list, miu_list)
 
 # plot
 fig = plt.figure()
 
 x, y = np.meshgrid(variables[0], variables[1])
-# pos = np.empty(x.shape + (2,))
-# pos[:, :, 0] = x
-# pos[:, :, 1] = y
-# print(z([1,2]))
+
 
 pos=np.dstack((x, y))
 # z1=z(pos)
-# print(z1)
-ax =fig.add_subplot(projection='3d')
+# print(np.amax(z1))
+#
+ax = fig.add_subplot(projection='3d')
 ax.plot_surface(x,y, z(pos), cmap='viridis', linewidth=0)
-plt.show()
+# plt.show()
+
+F = lambda x:-z(x)
+
+
+# ____define objective function___
+lb = [-ul / 2] * n
+ub = [ul / 2] * n
+# _____1.b______
+np.random.seed(12)
+hc = BaseHC(F, ub=ub, lb=lb, epoch=10, problem_size=2)
+best_pos1, best_fit1, list_loss1 = hc.train()
+print("best_pos: ")
+print(best_pos1)
+print("best_fit: ")
+print(best_fit1)
+print("list_loss: ")
+print(list_loss1)
+for i in list_loss1:
+    print(i)
+
+# ____1.c____
+# 1.c
+
+print("1c")
+gwo = BaseGWO(obj_func=F, lb=lb, ub=ub, epoch=10, problem_size=2)
+best_pos1, best_fit1, list_loss1 = gwo.train()
+print("best_pos: ")
+print(best_pos1)
+print("best_fit: ")
+print(best_fit1)
+print("list_loss: ")
+for i in list_loss1:
+    print(i)
+
+# ___1.d___
+print("1d")
+
+n=10
+miu_list=[]
+variance_list=[]
+for i in range(m+1):
+    var = [np.random.uniform(w*ul/10,w*ul) for i in range(n)]
+    miu=[np.random.uniform(-d*ul/2,d*ul/2) for i in range(n)]
+    miu_list.append(miu)
+    variance_list.append(var)
+
+
+z = G(m, variance_list, miu_list)
+F = lambda x:-z(x)
+lb = [-ul / 2] * n
+ub = [ul / 2] * n
+# _____hc______
+np.random.seed(12)
+hc = BaseHC(F, ub=ub, lb=lb, epoch=50, problem_size=10)
+best_pos1, best_fit1, list_loss1 = hc.train()
+print("best_pos: ")
+print(best_pos1)
+print("best_fit: ")
+print(best_fit1)
+print("list_loss: ")
+print(list_loss1)
+for i in list_loss1:
+    print(i)
+
+# ____gwo____
+gwo = BaseGWO(obj_func=F, lb=lb, ub=ub, epoch=50, problem_size=10)
+best_pos1, best_fit1, list_loss1 = gwo.train()
+print("best_pos: ")
+print(best_pos1)
+print("best_fit: ")
+print(best_fit1)
+print("list_loss: ")
+for i in list_loss1:
+    print(i)
