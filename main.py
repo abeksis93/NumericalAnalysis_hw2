b1b24fe6256fc0a1f8d0dfe6a1d9b9146f499622
leah mashi <leahma@post.bgu.ac.il>
2021-05-13 23:57:39 +0300

    initial commit

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import math
 import random
 
 # import gaussian as gaussian
+import sys
+from io import StringIO
 import matplotlib.pyplot as plt
-import numpy
 import numpy as np
-from mealpy.math_based.HC import BaseHC
+from mealpy.math_based.HC import BaseHC, OriginalHC
 from mealpy.swarm_based.GWO import BaseGWO
 from scipy.stats import multivariate_normal
 
@@ -53,24 +54,26 @@ def G(m, variance_list, miu_list):
     max_val = 0
     for i in range(m + 1):
         g_lst.append(multivariate_normal(miu_list[i], np.diag(variance_list[i])))
-        if max_val < g_lst[i].pdf(miu_list[i]):
-            max_val = g_lst[i].pdf(miu_list[i])
-            f = g_lst[i]
+        if max_val < multivariate_normal(miu_list[i], np.diag(variance_list[i])).pdf(miu_list[i]):
+            max_val = multivariate_normal(miu_list[i], np.diag(variance_list[i])).pdf(miu_list[i])
+    g_list_mu = zip(g_lst, miu_list)
+    g_list_mu = sorted(g_list_mu, key=lambda g_mu: g_mu[0].pdf(g_mu[1]), reverse=True)
+    g_lst = [g_mu[0] for g_mu in g_list_mu]
 
-
-    return lambda x: sum(
-        r * (1 - 0.05 * i) * g_lst[i].pdf(x) if g_lst[i] != f else f.pdf(x) / max_val for i in range(m + 1))
+    # for j in range(len(g_lst)):
+    #     if j != 0:
+    #         print((r * (1 - 0.05 * (i - 1))) * g_lst[j].pdf(miu_list[j]))
+    #     else:
+    #         print(g_lst[0].pdf(miu_list[0]) / max_val)
+    return lambda x: sum((r * (1 - 0.05 * (i - 1))) * g_lst[i].pdf(x) if i != 0 else g_lst[0].pdf(x) / max_val for i in range(m + 1))
 
 
 nList = [5, 10, 20, 40]
 mList = [0, 5, 10, 20]
-
 rList = [0.1, 0.3, 0.6, 0.9]
 ulList = [10, 20, 30, 40]
 wList = [0.01, 0.03, 0.06, 0.09]
 dList = [0.25, 0.5, 0.75, 1.0]
-Wlist = []
-miu_list = []
 
 # parameter choose:
 n = 2
@@ -79,17 +82,22 @@ ul = ulList[1]
 r = rList[1]
 w = wList[1]
 d = dList[1]
+Wlist = []
 for i in range(0, 6):
     Wlist.append(random.random() * r)
 
+
 variance_list = []
+miu_list = []
+
 for i in range(0, m + 1):
-    miu_list.append([np.random.uniform(-d*ul/2,d*ul/2) for i in range(n)])
-    variance_list.append([np.random.uniform(w*ul/10,w*ul) for i in range(n)])
+    miu_list.append([np.random.uniform(-d*ul/2, d*ul/2) for i in range(n)])
+    variance_list.append([np.random.uniform(w*ul/10, w*ul) for i in range(n)])
 
 variables = []
 for i in range(0, n):
     variables.append(np.linspace(-ul / 2, ul / 2, 1000))
+
 
 # function
 z = G(m, variance_list, miu_list)
@@ -99,77 +107,102 @@ fig = plt.figure()
 
 x, y = np.meshgrid(variables[0], variables[1])
 
+pos = np.dstack((x, y))
+zMax = z(pos)
+print(np.amax(zMax))
 
-pos=np.dstack((x, y))
-# z1=z(pos)
-# print(np.amax(z1))
-#
+
 ax = fig.add_subplot(projection='3d')
-ax.plot_surface(x,y, z(pos), cmap='viridis', linewidth=0)
-# plt.show()
+ax.plot_surface(x, y, z(pos), cmap='viridis', linewidth=0)
+plt.show()
 
 F = lambda x:-z(x)
-
 
 # ____define objective function___
 lb = [-ul / 2] * n
 ub = [ul / 2] * n
+
 # _____1.b______
 np.random.seed(12)
-hc = BaseHC(F, ub=ub, lb=lb, epoch=10, problem_size=2)
+old_stdout = sys.stdout
+sys.stdout = mystdout = StringIO()
+
+hc = OriginalHC(F, ub=ub, lb=lb, verbose=True, epoch=10, problem_size=2)
 best_pos1, best_fit1, list_loss1 = hc.train()
-print("best_pos: ")
-print(best_pos1)
-print("best_fit: ")
-print(best_fit1)
-print("list_loss: ")
-print(list_loss1)
-for i in list_loss1:
-    print(i)
+
+sys.stdout = old_stdout
+ResultString = mystdout.getvalue()
+arrResultString = ResultString.split("\n")
+arrZString = [arrResultString[i].split(",") for i in range(10)]
+arrZin1 = [arrZString[i][1].split(":") for i in range(10)]
+arrZ = []
+arrIteration = []
+for i in range(10):
+    arrZ.append(float(arrZin1[i][1]))
+    arrIteration.append(i)
+plt.scatter(arrZ, arrIteration)
+plt.show()
 
 # ____1.c____
-# 1.c
-
 print("1c")
+np.random.seed(12)
+old_stdout = sys.stdout
+sys.stdout = mystdout = StringIO()
+
 gwo = BaseGWO(obj_func=F, lb=lb, ub=ub, epoch=10, problem_size=2)
 best_pos1, best_fit1, list_loss1 = gwo.train()
-print("best_pos: ")
-print(best_pos1)
-print("best_fit: ")
-print(best_fit1)
-print("list_loss: ")
-for i in list_loss1:
-    print(i)
+
+sys.stdout = old_stdout
+ResultString = mystdout.getvalue()
+arrResultString = ResultString.split("\n")
+arrZString = [arrResultString[i].split(",") for i in range(10)]
+arrZin1 = [arrZString[i][1].split(":") for i in range(10)]
+arrZ = []
+arrIteration = []
+for i in range(10):
+    arrZ.append(float(arrZin1[i][1]))
+    arrIteration.append(i)
+plt.scatter(arrZ, arrIteration)
+plt.show()
+
 
 # ___1.d___
 print("1d")
 
-n=10
-miu_list=[]
-variance_list=[]
+n = 10
+miu_list = []
+variance_list = []
 for i in range(m+1):
-    var = [np.random.uniform(w*ul/10,w*ul) for i in range(n)]
-    miu=[np.random.uniform(-d*ul/2,d*ul/2) for i in range(n)]
+    var = [np.random.uniform(w*ul/10, w*ul) for i in range(n)]
+    miu = [np.random.uniform(-d*ul/2, d*ul/2) for i in range(n)]
     miu_list.append(miu)
     variance_list.append(var)
 
 
 z = G(m, variance_list, miu_list)
 F = lambda x:-z(x)
-lb = [-ul / 2] * n
+lb = [-ul / 2]
 ub = [ul / 2] * n
 # _____hc______
 np.random.seed(12)
-hc = BaseHC(F, ub=ub, lb=lb, epoch=50, problem_size=10)
+old_stdout = sys.stdout
+sys.stdout = mystdout = StringIO()
+
+hc = OriginalHC(F, ub=ub, lb=lb, epoch=50, problem_size=10)
 best_pos1, best_fit1, list_loss1 = hc.train()
-print("best_pos: ")
-print(best_pos1)
-print("best_fit: ")
-print(best_fit1)
-print("list_loss: ")
-print(list_loss1)
-for i in list_loss1:
-    print(i)
+
+sys.stdout = old_stdout
+ResultString = mystdout.getvalue()
+arrResultString = ResultString.split("\n")
+arrZString = [arrResultString[i].split(",") for i in range(50)]
+arrZin1 = [arrZString[i][1].split(":") for i in range(50)]
+arrZ = []
+arrIteration = []
+for i in range(50):
+    arrZ.append(float(arrZin1[i][1]))
+    arrIteration.append(i)
+plt.scatter(arrZ, arrIteration)
+plt.show()
 
 # ____gwo____
 gwo = BaseGWO(obj_func=F, lb=lb, ub=ub, epoch=50, problem_size=10)
